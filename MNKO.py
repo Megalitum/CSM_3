8ae4d059137da10359a39433a23e71f2300a5b01
shooter <shooter.il@yandex.ua>
2016-04-01 11:55:00 +0300

    begin mnko

---
+++ MNKO.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def mnko(x, y):
+    """
+    realise MNKO
+    :param x: matrix X
+    :param y: vector y
+    :return: vector Theta
+    """
+
+    Hinv = []
+    Hinv.append(np.array([0], dtype = float))
+    
