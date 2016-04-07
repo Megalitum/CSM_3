5ab8b6ad5bff140171761416f99bd971ec6c7e2d
Megalitum <fatvlad.95@gmail.com>
2016-04-07 16:05:02 +0300

    Fixed matplotlib qt conflict on OS X.

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 # coding: utf8
 
 import sys
+import matplotlib
+matplotlib.use("Qt5Agg", force=True)
 from PyQt5.QtWidgets import QApplication
 from main_window import MainWindow
+
+import matplotlib.pyplot as plt
 
 app = QApplication(sys.argv)
 app.setApplicationName('CSM_3')
 # -----------------------------------------------------#
-
 form = MainWindow()
 form.setWindowTitle('Лабораторная работа №3')
 form.show()
